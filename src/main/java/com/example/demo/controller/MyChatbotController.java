48d980ce07dc3e7ef109f81b1b0570497d0b9dc3
Deepak Mandal <demanda1@in.ibm.com>
2019-12-30 14:18:27 +0530

    cors

--- src/main/java/com/example/demo/controller/MyChatbotController.java
+++ src/main/java/com/example/demo/controller/MyChatbotController.java
@@ -1,7 +1,15 @@
 package com.example.demo.controller;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,7 +22,17 @@ import com.example.demo.service.MyChatbotService;
 public class MyChatbotController {
 	@Autowired MyChatbotService chatbotservice;
 	@GetMapping("/send/{msg}")
-	public Object sendMessage(@PathVariable String msg) {
+	public Object sendMessage(@PathVariable String msg,ServletRequest req, ServletResponse res, FilterChain chain)  throws IOException, ServletException{
+		HttpServletRequest request = (HttpServletRequest) req;
+	    HttpServletResponse response = (HttpServletResponse) res;
+
+	    response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+	    response.setHeader("Access-Control-Allow-Credentials", "true");
+	    response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+	    response.setHeader("Access-Control-Max-Age", "3600");
+	    response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
+
+	    chain.doFilter(req, res);
 		System.out.println(msg);
 		List<String> m=new ArrayList<String>();
 		String message=chatbotservice.botService(msg);
