0cd15fd59b836e140731dcc0355431a035e5fb9c
Deepak Mandal <demanda1@in.ibm.com>
2019-12-31 14:35:24 +0530

    make it better again

--- src/main/java/com/example/demo/MyChatbotApplication.java
+++ src/main/java/com/example/demo/MyChatbotApplication.java
@@ -1,13 +1,21 @@
 package com.example.demo;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.service.MyChatbotService;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -20,7 +28,16 @@ public class MyChatbotApplication {
 	  public String home() {
 	    return "This is a trivial service that demonstrates how a Eureka Client can register with a Eureka Server";
 	  }
-
+	  
+	  @Autowired MyChatbotService chatbotservice;
+		@GetMapping("/send/{msg}")
+		public Object sendMessage(@PathVariable String msg){
+			System.out.println(msg);
+			List<String> m=new ArrayList<String>();
+			String message=chatbotservice.botService(msg);
+			m.add(message);
+			return m;
+		}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyChatbotApplication.class, args);
