e0ed2df7d371e3e06a99018e9d3dffd787673cce
Deepak Mandal <demanda1@in.ibm.com>
2019-12-31 13:00:52 +0530

    main file

--- src/main/java/com/example/demo/MyChatbotApplication.java
+++ src/main/java/com/example/demo/MyChatbotApplication.java
@@ -1,12 +1,24 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@Configuration
+@EnableAutoConfiguration
+@EnableEurekaClient
 public class MyChatbotApplication {
+	  @RequestMapping("/")
+	  public String home() {
+	    return "This is a trivial service that demonstrates how a Eureka Client can register with a Eureka Server";
+	  }
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyChatbotApplication.class, args);
